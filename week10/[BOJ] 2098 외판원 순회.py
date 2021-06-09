5129e5e04cb6d9b17c290dae02c41d6cdb78d535
vitriol95 <vitriol95@snu.ac.kr>
2021-06-09 18:01:58 +0900

    0609

--- week10/[BOJ] 2098 외판원 순회.py
+++ week10/[BOJ] 2098 외판원 순회.py
@@ -1,27 +1,27 @@
 import sys
-N = int(sys.stdin.readline())
-W = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
-dp = [[0] * (1 << N - 1) for _ in range(N)]
+n = int(sys.stdin.readline())
+graph = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+dp = [[0] * (1 << n - 1) for _ in range(n)]
 
 def solution(i, route):
     
     if dp[i][route] != 0:
         return dp[i][route]
 
-    if route == (1 << (N - 1)) - 1:
-        if W[i][0]:
-            return W[i][0]
+    if route == (1 << (n - 1)) - 1:
+        if graph[i][0]:
+            return graph[i][0]
         else:
-            return float('inf')
+            return 1e9
 
-    min_dist = float('inf')
+    min_dist = 1e9
     
-    for j in range(1, N):
-        if not W[i][j]:
+    for j in range(1, n):
+        if not graph[i][j]:
             continue
         if route & (1 << j - 1):
             continue
-        dist = W[i][j] + solution(j, route | (1 << (j - 1)))
+        dist = graph[i][j] + solution(j, route | (1 << (j - 1)))
         if min_dist > dist:
             min_dist = dist
     dp[i][route] = min_dist
